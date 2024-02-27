ee41e52338aa05a6b7efee57dcabd3ffaa8b923f
edgmed <edgar.medinazavala@mercadolibre.com>
2024-02-27 13:05:23 -0600

    Inclusion Test0001

--- src/test/java/com/bootcamp/be_java_hisp_w25_g14/T0001/ServiceTest.java
+++ src/test/java/com/bootcamp/be_java_hisp_w25_g14/T0001/ServiceTest.java
@@ -1,10 +1,7 @@
-package com.bootcamp.be_java_hisp_w25_g14;
+package com.bootcamp.be_java_hisp_w25_g14.T0001;
 
-import com.bootcamp.be_java_hisp_w25_g14.exceptions.FollowException;
 import com.bootcamp.be_java_hisp_w25_g14.exceptions.NotFoundException;
-import com.bootcamp.be_java_hisp_w25_g14.repository.IUserRepo;
 import com.bootcamp.be_java_hisp_w25_g14.repository.UserRepoImp;
-import com.bootcamp.be_java_hisp_w25_g14.service.IUserService;
 import com.bootcamp.be_java_hisp_w25_g14.service.UserServiceImp;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,11 +11,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.*;
-import static org.mockito.internal.configuration.GlobalConfiguration.validate;
 
 @ExtendWith(MockitoExtension.class)
 
-public class T0001 {
+public class ServiceTest {
     @Mock
     private UserRepoImp userRepo;
 
@@ -35,14 +31,6 @@ public class T0001 {
         userService.addFollowe(userId,userIdToFollow);
         verify(userRepo, atLeast(1)).addFollower(userId,userIdToFollow);
     }
-    @Test
-    void followUserNoExists(){
-        //Arrange
-        Integer userId=10;
-        Integer userIdToFollow=3;
 
-        //Act Assert
-        assertThrows(NotFoundException.class, () -> userService.addFollowe(userId,userIdToFollow));
-    }
 
 }
