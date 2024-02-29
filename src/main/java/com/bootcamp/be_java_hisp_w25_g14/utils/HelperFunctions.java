ea6472bc48685e40e9b3f3ed10287c3f6cd6e514
Pablo Mieres <pablo.mieresnoriega@mercadolibre.com.mx>
2024-02-29 16:29:04 -0600

    Tests de Integracion Individuales

--- src/main/java/com/bootcamp/be_java_hisp_w25_g14/utils/HelperFunctions.java
+++ src/main/java/com/bootcamp/be_java_hisp_w25_g14/utils/HelperFunctions.java
@@ -1,7 +1,9 @@
 package com.bootcamp.be_java_hisp_w25_g14.utils;
 
+import com.bootcamp.be_java_hisp_w25_g14.dto.MessageDto;
 import com.bootcamp.be_java_hisp_w25_g14.dto.PostDto;
-import com.bootcamp.be_java_hisp_w25_g14.entity.Post;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.util.Comparator;
 import java.util.List;
@@ -15,5 +17,10 @@ public class HelperFunctions {
     public static List<PostDto> sortPostsByDateDescending(List<PostDto> list){
         list.sort(Comparator.comparing(PostDto::getDate, Comparator.reverseOrder()));
         return list;
+    }
+
+    public static String serializeObject(Object obj) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString( obj );
     }
 }
