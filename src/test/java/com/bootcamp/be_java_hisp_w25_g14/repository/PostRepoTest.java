9e26db2e445364e7d4df0d7bb5ac0238edd87eef
Pablo Mieres <pablo.mieresnoriega@mercadolibre.com.mx>
2024-03-01 09:01:46 -0600

    Borrar archivo innecesario

--- src/test/java/com/bootcamp/be_java_hisp_w25_g14/repository/PostRepoTest.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.be_java_hisp_w25_g14.repository;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public class PostRepoTest {
-}
